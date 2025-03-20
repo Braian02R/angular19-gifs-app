8c109c5ccd90f20a3e4b36aa998c28dd32766a56
Braian <bf02rojas.edu@gmail.com>
2025-03-20 19:22:40 -0300

    Mostrar imágenes de relleno + separación de componentes más pequeños

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GifListComponent } from "../../components/gifs-list/gif-list.component";
 
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent { }
