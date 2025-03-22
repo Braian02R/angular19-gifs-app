b30593012dc767fbef9b004e45be9941706c099b
Braian <bf02rojas.edu@gmail.com>
2025-03-22 12:48:57 -0300

    finalizando petición HTTP GET: gif interface personalizada + gif mapper + creación de señal para trending gifs + inyección de gif service en componente trending

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../services/gif.service';
 
 //temporalmente
 const imageUrls: string[] = [
@@ -24,4 +25,6 @@ const imageUrls: string[] = [
 })
 export default class TrendingPageComponent { 
   gifs= signal(imageUrls);
+
+  gifsService = inject( GifService );
 }
